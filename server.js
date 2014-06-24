48f074a1a36c5a07693667c430ea8bb4d369b162
max <ternovenko@gmail.com>
2014-06-24 16:53:23 +0300

    I2C and light sensor some fix

--- server.js
+++ server.js
@@ -1,47 +1,45 @@
 var Gpio = require('onoff').Gpio,
-	led = new Gpio(14, 'out');
-
+	led = new Gpio(14, 'out'),
+	gigrometr = new Gpio(15, 'in');
 
 var i2c = require('i2c');
 var address = 0x48;
 var wire = new i2c(address, {device: '/dev/i2c-1', debug: false}); // point to your i2c address, debug provides REPL interface
 
-wire.writeByte(3,function(err){
-	if(err){
-		console.log(err);
-		 throw err;
-	}
 
-	setInterval(function(){
-		wire.readBytes(0, 2, function(err, res) {
-			console.log(err);
-			console.log(res[1]);
-
-			 led.write(res[1]<215? 0:1);
-	 		// result contains a buffer of bytes
+	setInterval(function () {
+		wire.readBytes(0, 2, function (err, res) {
+			if (err){
+				console.log(err);
+				throw err;
+			}
+			console.log("Analog gigrometer say: %d", res[1]);
 		});
-	},500);
+
+	}, 500);
+
+setInterval(function () {
+	gigrometr.read(function (err, value) {
+		if (err) throw err;
+		console.log("Digital gigrometer say: %s", value);
+	});
+
+}, 500);
+
+setInterval(function () {
+	led.read(function (err, value) {
+		if (err) throw err;
+
+		led.write(value == 0 ? 1 : 0, function (err) {
+			if (err) throw err;
+		});
+	});
+
+},1000);
 
 
+
+process.on('SIGINT', function exit() {
+	led.unexport();
+	process.exit();
 });
-
-
-
-
-
-//	setInterval(function(){
-//		led.read(function(err, value){
-//			if(err) throw err;
-//			console.log('fun %s', value);
-//			led.write(value ? 0:1);
-//		});
-//
-//	},1000);
-//
-//Some
-
-
- process.on('SIGINT', function exit() {
- 	led.unexport();
- 	process.exit();
- });
